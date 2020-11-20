96c1c1377d79cb7620dfef306480c39f19f6b49f
Nikiro <56888784+NikitaRogozhnikov@users.noreply.github.com>
2020-11-20 16:02:36 +0300

    2

--- BSTask/settings.py
+++ BSTask/settings.py
@@ -88,7 +88,7 @@ CHANNEL_LAYERS = {
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -106,6 +106,7 @@ DATABASES = {
     'PORT': '5432',
  }
 }
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
