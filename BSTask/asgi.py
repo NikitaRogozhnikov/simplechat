badb6f453c900516379bc3baa6302ad81a338e8e
Nikiro <56888784+NikitaRogozhnikov@users.noreply.github.com>
2020-11-24 00:04:28 +0300

    some changes

--- BSTask/asgi.py
+++ BSTask/asgi.py
@@ -6,25 +6,6 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
-'''
-import os
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-import Chat.routing
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BSTask.settings")
-
-application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
-        URLRouter(
-            Chat.routing.websocket_urlpatterns
-        )
-    ),
-})
-'''
 import os
 import django
 from channels.routing import get_default_application
@@ -33,4 +14,3 @@ from channels.layers import get_channel_layer
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BSTask.settings')
 django.setup()
 application = get_default_application()
-channel_layer = get_channel_layer()
